c1155b3eed0a73f3d4443ba7901678e419a3ac59
scrabill <shannon.crabill@gmail.com>
2020-06-15 18:02:19 -0400

    Array of emojis for cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,55 @@
-console.log("wow")
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+    const cardArray = [
+      {
+        name: "fries",
+        img: "🍟"
+      },
+      {
+        name: "fries",
+        img: "🍟"
+      },
+      {
+        name: "cheeseburger",
+        img: "🍔"
+      }
+      ,
+      {
+        name: "cheeseburger",
+        img: "🍔"
+      },
+      {
+        name: "hotdog",
+        img: "🌭"
+      },
+      {
+        name: "hotdog",
+        img: "🌭"
+      },
+      {
+        name: "icecream",
+        img: "🍦"
+      },
+      {
+        name: "icecream",
+        img: "🍦"
+      },
+      {
+        name: "pizza",
+        img: "🍕"
+      },
+      {
+        name: "pizza",
+        img: "🍕"
+      },
+      {
+        name: "cake",
+        img: "🍰"
+      },
+      {
+        name: "cake",
+        img: "🍰"
+      }
+    ]
+});
